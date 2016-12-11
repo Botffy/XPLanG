8936926bfecdc766df4192d9269d3c5ca201fa1a
Ármin Scipiades <scipiades.armin@gmail.com>
2016-12-11 23:45:31 +0100

    Typecheck array indexing

--- src/main/java/ppke/itk/xplang/lang/LValueParser.java
+++ src/main/java/ppke/itk/xplang/lang/LValueParser.java
@@ -3,15 +3,20 @@ package ppke.itk.xplang.lang;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ppke.itk.xplang.ast.*;
+import ppke.itk.xplang.common.Location;
+import ppke.itk.xplang.common.Translator;
 import ppke.itk.xplang.parser.ParseError;
 import ppke.itk.xplang.parser.Parser;
 import ppke.itk.xplang.parser.Token;
+import ppke.itk.xplang.parser.TypeError;
+import ppke.itk.xplang.type.Scalar;
 
 /**
  * {@code LValue = IDENTIFIER [BRACKET_OPEN INT_LITERAL BRACKET_CLOSE] }
  */
 class LValueParser {
     private final static Logger log = LoggerFactory.getLogger("Root.Parser.Grammar");
+    private final static Translator translator = Translator.getInstance("Plang");
 
     private LValueParser() { /* empty private ctor */ }
 
@@ -22,7 +27,14 @@ class LValueParser {
         LValue Result = parser.context().getVariableReference(var);
         if(parser.actual().symbol().equals(PlangSymbol.BRACKET_OPEN.symbol())) {
             parser.advance();
+            Location location = parser.actual().location();
             RValue address = RValueParser.parse(parser);
+            if(!Scalar.INTEGER_TYPE.accepts(address.getType())) {
+                throw new TypeError(
+                    translator.translate("plang.array_indextype_mismatch", address.getType()),
+                    location
+                );
+            }
             parser.accept(PlangSymbol.BRACKET_CLOSE.symbol());
             Result = new ElementRef(toRValue(Result), address);
         }
