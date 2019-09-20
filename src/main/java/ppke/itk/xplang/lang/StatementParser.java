a15c9a1691e14d8f45bf87d1e54766d204a2bedd
Ármin Scipiades <scipiades.armin@gmail.com>
2019-09-20 12:54:09 +0200

    Refactor StatementParser to be more declarative

--- src/main/java/ppke/itk/xplang/lang/StatementParser.java
+++ src/main/java/ppke/itk/xplang/lang/StatementParser.java
@@ -8,43 +8,50 @@ import ppke.itk.xplang.parser.Parser;
 import ppke.itk.xplang.parser.Symbol;
 import ppke.itk.xplang.parser.SyntaxError;
 
-import static java.util.Arrays.asList;
+import java.util.EnumMap;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toSet;
 
 /**
- * {@code Statement = Assignment | Conditional | Loop | InputStatement | OutputStatement | OpenStatement }
+ * {@code Statement = Assignment | Conditional | Loop | InputStatement | OutputStatement | OpenStatement | CloseStatement }
  */
 final class StatementParser {
     private final static Logger log = LoggerFactory.getLogger("Root.Parser.Grammar");
+
+    private static final Map<PlangSymbol, StatementParserFunction> statementParsers = new EnumMap<>(PlangSymbol.class);
+    static {
+        statementParsers.put(PlangSymbol.IDENTIFIER, AssignmentParser::parse);
+        statementParsers.put(PlangSymbol.IF, ConditionalParser::parse);
+        statementParsers.put(PlangSymbol.LOOP, LoopParser::parse);
+        statementParsers.put(PlangSymbol.IN, InputStatementParser::parse);
+        statementParsers.put(PlangSymbol.OUT, OutputStatementParser::parse);
+        statementParsers.put(PlangSymbol.OPEN, OpenStatementParser::parse);
+        statementParsers.put(PlangSymbol.CLOSE, CloseStatementParser::parse);
+    }
 
     private StatementParser() { /* empty private ctor */ }
 
     static Statement parse(Parser parser) throws ParseError {
         log.debug("Statement");
         Symbol act = parser.actual().symbol();
-        if(act.equals(parser.symbol(PlangSymbol.IDENTIFIER))) {
-            return AssignmentParser.parse(parser);
-        } else if(act.equals(parser.symbol(PlangSymbol.IF))) {
-            return ConditionalParser.parse(parser);
-        } else if (act.equals(parser.symbol(PlangSymbol.LOOP))) {
-            return LoopParser.parse(parser);
-        } else if (act.equals(parser.symbol(PlangSymbol.IN))) {
-            return InputStatementParser.parse(parser);
-        } else if (act.equals(parser.symbol(PlangSymbol.OUT))) {
-            return OutputStatementParser.parse(parser);
-        } else if (act.equals(parser.symbol(PlangSymbol.OPEN))) {
-            return OpenStatementParser.parse(parser);
-        } else if (act.equals(parser.symbol(PlangSymbol.CLOSE))) {
-            return CloseStatementParser.parse(parser);
+        PlangSymbol symbol = PlangSymbol.valueOf(act.getName());
+
+        if (!statementParsers.containsKey(symbol)) {
+            throw new SyntaxError(
+                statementParsers.keySet().stream()
+                    .map(x -> parser.symbol(symbol))
+                    .collect(toSet()),
+                act, parser.actual()
+            );
         }
 
-        throw new SyntaxError(asList(
-            parser.symbol(PlangSymbol.IDENTIFIER),
-            parser.symbol(PlangSymbol.IF),
-            parser.symbol(PlangSymbol.LOOP),
-            parser.symbol(PlangSymbol.OUT),
-            parser.symbol(PlangSymbol.IN),
-            parser.symbol(PlangSymbol.OPEN),
-            parser.symbol(PlangSymbol.CLOSE)
-        ), act, parser.actual());
+        StatementParserFunction statementParser = statementParsers.get(symbol);
+        return statementParser.parse(parser);
+    }
+
+    @FunctionalInterface
+    private interface StatementParserFunction {
+        Statement parse(Parser parser) throws ParseError;
     }
 }
