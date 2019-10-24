a46d4d6b4e813b28f51c7561d725b72505f8d5ec
Ármin Scipiades <scipiades.armin@gmail.com>
2019-10-24 18:57:48 +0200

    Prepare LValueParser for field access

--- src/main/java/ppke/itk/xplang/lang/LValueParser.java
+++ src/main/java/ppke/itk/xplang/lang/LValueParser.java
@@ -9,6 +9,9 @@ import ppke.itk.xplang.common.Location;
 import ppke.itk.xplang.parser.*;
 import ppke.itk.xplang.type.Archetype;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static ppke.itk.xplang.lang.PlangName.name;
 
 /**
@@ -16,6 +19,10 @@ import static ppke.itk.xplang.lang.PlangName.name;
  */
 final class LValueParser {
     private final static Logger log = LoggerFactory.getLogger("Root.Parser.Grammar");
+    private final static Map<Symbol, Parselet> parselets = new HashMap<>();
+    static {
+        parselets.put(Symbol.BRACKET_OPEN, LValueParser::parseElementAccess);
+    }
 
     private LValueParser() { /* empty private ctor */ }
 
@@ -23,29 +30,38 @@ final class LValueParser {
         log.debug("LValue");
         Token token = parser.accept(Symbol.IDENTIFIER);
 
-        LValue Result = parser.context().getVariableReference(name(token.lexeme()), token);
-        while(parser.actual().symbol().equals(Symbol.BRACKET_OPEN)) {
-            if (Result.getType().indexType().equals(Archetype.NONE)) {
-                throw new ParseError(Result.location(), ErrorCode.TYPE_MISMATCH_NOT_ADDRESSABLE, Result.getType());
-            }
-
-            Token startToken = parser.advance();
-
-            Expression indexExpression = parser.parseExpression();
-            RValue index = TypeChecker.in(parser.context())
-                .checking(indexExpression)
-                .expecting(Result.getType().indexType())
-                .withCustomErrorMessage(
-                    node -> new ParseError(node.location(), ErrorCode.TYPE_MISMATCH_ARRAY_INDEX, node.getType())
-                ).build()
-                .resolve();
-
-            Token endToken = parser.accept(Symbol.BRACKET_CLOSE);
-            Result = new ElementRef(
-                new Location(startToken.location().start, endToken.location().end),
-                Result.toRValue(), index
-            );
+        LValue result = parser.context().getVariableReference(name(token.lexeme()), token);
+        while(parselets.containsKey(parser.actual().symbol())) {
+            result = parselets.get(parser.actual().symbol()).parse(result, parser);
         }
-        return Result;
+        return result;
+    }
+
+    private static LValue parseElementAccess(LValue base, Parser parser) throws ParseError {
+        if (!base.getType().indexType().accepts(parser.context().integerType())) {
+            throw new ParseError(base.location(), ErrorCode.TYPE_MISMATCH_NOT_ADDRESSABLE, base.getType());
+        }
+
+        Token startToken = parser.advance();
+
+        Expression indexExpression = parser.parseExpression();
+        RValue index = TypeChecker.in(parser.context())
+            .checking(indexExpression)
+            .expecting(base.getType().indexType())
+            .withCustomErrorMessage(
+                node -> new ParseError(node.location(), ErrorCode.TYPE_MISMATCH_ARRAY_INDEX, node.getType())
+            ).build()
+            .resolve();
+
+        Token endToken = parser.accept(Symbol.BRACKET_CLOSE);
+        return new ElementRef(
+            new Location(startToken.location().start, endToken.location().end),
+            base.toRValue(), index
+        );
+    }
+
+    @FunctionalInterface
+    private static interface Parselet {
+        LValue parse(LValue base, Parser parser) throws ParseError;
     }
 }
